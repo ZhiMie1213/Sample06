80e9acbeeec768f21ae82d8749cba5ac8ac2df3a
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2022-10-06 15:54:53 +0900

    2022/10/06

---
+++ SampleCode06/Phones.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode06 {
+    public class Phones {
+        public Phones( string userName ) {
+            UserName = userName;
+        }
+        public string UserName {
+            get; set;
+        }
+
+        public List<SmartPhone> MySmartPhones { get; set; } = new List<SmartPhone>( );
+    }
+
+    public class SmartPhone {
+        public string OS {
+            get; set;
+        }
+        public string ModelName {
+            get; set;
+        }
+        public string CPU {
+            get; set;
+        }
+        public int RAM {
+            get; set;
+        }
+        public int ROM {
+            get; set;
+        }
+        public SmartPhone( string os, string modelname, string cpu, int ram, int rom ) {
+            OS = os;
+            ModelName = modelname;
+            CPU = cpu;
+            RAM = ram;
+            ROM = rom;
+        }
+    }
+
+}
---
+++ SampleCode06/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SampleCode06 {
+    public class Program {
+        public static void Main( string[ ] args ) {
+            Phones phones = new Phones( "ben" );
+
+            phones.
+        }
+    }
+}
