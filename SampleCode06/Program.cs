538c171e6877276fb43b058844a8a45487acff05
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2022-10-13 13:22:02 +0900

    2022/10/13

--- SampleCode06/Program.cs
+++ SampleCode06/Program.cs
@@ -1,10 +1,31 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace SampleCode06 {
     public class Program {
         public static void Main( string[ ] args ) {
+            Deserialize( );
+        }
+
+        private const string JsonFilePath = @"C:\Users\22415\OneDrive\桌面\text.jon";
+
+        private static void Serialize( ) {
             Phones phones = new Phones( "ben" );
 
-            phones.
+            phones.MySmartPhones.Add( new SmartPhone( "iOS", "iPhone14ProMax", "A16", 6, 512 ) );
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject( phones );
+
+            System.IO.File.WriteAllText( JsonFilePath, json );
+
+            Console.WriteLine( "出力完了" );
+        }
+
+        private static void Deserialize( ) {
+            string json = System.IO.File.ReadAllText( JsonFilePath );
+
+            Phones phones = Newtonsoft.Json.JsonConvert.DeserializeObject<Phones>(json);
+
+            Console.WriteLine( phones.MySmartPhones[0].ROM );
         }
     }
 }
